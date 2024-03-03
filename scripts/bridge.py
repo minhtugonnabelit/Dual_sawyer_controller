49bd49d343f81acecb73fc072be3982441122367
Anh Minh Tu <anhminh.tu@student.uts.edu.au>
2024-03-03 21:06:53 +1100

    Refactored vel_joystick node

--- scripts/bridge.py
+++ scripts/bridge.py
@@ -2,14 +2,6 @@ import roslibpy
 import threading
 import time
 
-# This script is used to open the commnuication between the two clients hosted via rosbridge server
-# The protocol is as follows:
-# 1. Both rosbridge server are running on two different machines (distinct IP) at background
-# 2. The script will initiate the client to the each rosbridge server
-# 3. Then custom topics will be created based on the desired data that need to be exchanged between the closed loop controller node running on each machine.
-# 4. Data retrieved from the server will be centralized into the machine running this script and will be published to the other machines to used for multiple
-#    purposes such as: collision avoidance, singularity avoidance, reachability check, etc.
-
 class Bridge:
 
     """
@@ -51,24 +43,5 @@ class Bridge:
     # TODO:
         # Handle the tf listenner that can make the communication channel to be more efficient (without the need of decompress image data retrieved from the server socker).
         # Setup the architecture to seemlessly callback data sent between controller and publish them to the other controller
-
-
-    def pub_topics_init(self, names):
-        """
-        This function is used to initialize the topics to be publish that will be used to exchange data between the two clients
-
-        Possible topics to be published:
-        - for endpoint location feedback: /robot/limb/right/endpoint_state
-        - for indicator between picking and clamping state /mission_state
-        - for image subscribed /camera/rgb/image_raw
-        - for tf listenner /tf
-
-        """
-        self._endpoint_pub = roslibpy.Topic(self._client, '/robot/limb/right/endpoint_state', 'intera_core_msgs/EndpointState')
-        self._endpoint_sub = roslibpy.Topic(self._client, '/robot/limb/left/endpoint_state', 'intera_core_msgs/EndpointState')
-        self._mission_pub = roslibpy.Topic(self._client, '/mission_state', 'std_msgs/Bool')
-        self._mission_sub = roslibpy.Topic(self._client, '/mission_state', 'std_msgs/Bool')
-        self._endpoint_pub.advertise()
-        self._endpoint_sub.subscribe(self.endpoint_state_callback)
-        self._mission_pub.advertise()
-        self._mission_sub.subscribe(self.mission_state)
+   
+        # 
--- scripts/test/vel_joystick.py
+++ scripts/test/vel_joystick.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 import rospy
 from sensor_msgs.msg import JointState, Joy
@@ -7,64 +6,30 @@ import intera_interface
 from intera_interface import CHECK_VERSION
 from intera_core_msgs.msg import JointCommand
 
-# from dual_sawyer_controller import Sawyer
+import numpy as np
+from sawyerRTB import Sawyer
 
 POSITION_MODE = int(1)
 VELOCITY_MODE = int(2)
 TORQUE_MODE = int(3)
 TRAJECTORY_MODE = int(4)
 
-import roboticstoolbox as rtb
 
-class Sawyer(rtb.DHRobot):
+def solve_RMRC(jacob, ee_vel, w_thresh=0.04, max_damp=0.5):
 
-    def __init__(self) -> None:
-        super().__init__(
-            self._create_DH(),
-            name='Sawyer',
-            manufacturer='Rethink Robotics',
-            keywords=('dynamics', 'symbolic'),
-            )
-        
-        self.q = [0.0, -0.9, 0.0, 1.8, 0.0, -0.9, 0.0]
+    # calculate manipulability
+    w = np.sqrt(np.linalg.det(jacob @ np.transpose(jacob)))
 
-    def _create_DH(self):
-        """
-        Create robot's standard DH model
-        """
+    # if manipulability is less than threshold, add damping
+    damp = (1 - np.power(w/w_thresh, 2)) * max_damp if w < w_thresh else 0
 
-        # deg = np.pi / 180
-        mm = 1e-3
-        # kinematic parameters
-        a = np.r_[81, 0, 0, 0, 0, 0, 0] * mm
-        d = np.r_[317, 192.5, 400, -168.5, 400, 136.3, 133.75] * mm
-        alpha = [-np.pi / 2, 
-                 np.pi / 2, 
-                 -np.pi / 2, 
-                 np.pi / 2, 
-                 -np.pi / 2, 
-                 np.pi / 2, 
-                 0]
-        qlim = np.deg2rad([[-175, 175],
-                           [-219, 131],
-                           [-175, 175],
-                           [-175, 175],
-                           [-170.5, 170.5],
-                           [-170.5, 170.5],
-                           [-270, 270]])
+    # calculate damped least square
+    j_dls = np.transpose(jacob) @ np.linalg.inv( jacob @ np.transpose(jacob) + damp * np.eye(6) )
 
-        # offset to have the dh from toolbox match with the actual pose
-        offset = [0, np.pi/2, 0, 0, 0, 0, -np.pi/2]
+    # get joint velocities, if robot is in singularity, use damped least square
+    joint_vel = j_dls @ np.transpose(ee_vel)
 
-        links = []
-
-        for j in range(7):
-            link = rtb.RevoluteDH(
-                d=d[j], a=a[j], alpha=alpha[j], offset=offset[j], qlim=qlim[j])
-            links.append(link)
-
-        return links
-
+    return joint_vel
 
 class VelCtrl:
 
@@ -84,7 +49,7 @@ class VelCtrl:
         rospy.Subscriber("/robot/joint_states", JointState, self.js_callback)
 
         # Velocity Control Message Publisher
-        self.joint_command = VelCtrl.joint_command_to_msg()
+        self.joint_command = VelCtrl.joint_command_init()
         self._joint_comm_pub = rospy.Publisher(
             "/robot/limb/right/joint_command", JointCommand, queue_size=10
         )
@@ -104,73 +69,7 @@ class VelCtrl:
             # If the current joint configurations of the robot are set to 0 put the thread to sleep (similar to a rate_limiter.sleep())
             rospy.sleep(0.1)
 
-
-    def gripper_ctrl_init(self):
-        """
-        """
-
-        # Initialise interfaces
-        rs = intera_interface.RobotEnable(CHECK_VERSION)
-        init_state = rs.state()
-        gripper = None
-        original_deadzone = None
-
-        rp = intera_interface.RobotParams()
-        valid_limbs = rp.get_limb_names()
-
-        # Cleaning function (executed on shutdown)
-        def clean_shutdown():
-            if gripper and original_deadzone:
-                gripper.set_dead_zone(original_deadzone)
-
-        try:
-            # Instantiate the gripper object
-            gripper = intera_interface.Gripper(
-                valid_limbs[0] + "_gripper")
-        except (ValueError, OSError) as e:
-            rospy.logerr(
-                "Could not detect an electric gripper attached to the robot.")
-            clean_shutdown()
-            return
-        rospy.on_shutdown(clean_shutdown)
-
-        # Possible deadzone values: 0.001 - 0.002
-        original_deadzone = gripper.get_dead_zone()
-        gripper.set_dead_zone(0.001)
-
-        return gripper
-
-    @staticmethod
-    def joint_command_to_msg():
-
-        # Joint Command Message Initialisation
-        joint_command_msg = JointCommand()
-
-        # To check the order of the joints run a 'rostopic echo /robot/joint_states', and assign the order displayed to the JointCommand().names argument
-        # This sequence is important as it is used to map the joint velocities to the correct joints, currently matched with actual Sawyer model instead of Gazebo version.
-        joint_command_msg.names = [
-            "head_pan",
-            "right_j0",
-            "right_j1",       
-            "right_j2",
-            "right_j3",
-            "right_j4",
-            "right_j5",
-            "right_j6",
-            "torso_t0",
-        ]
-
-        # Set the control mode to the VELOCITY
-        joint_command_msg.mode = VELOCITY_MODE
-
-        # Set the initial velocity at all joints to 0.0
-        joint_command_msg.velocity = np.ndarray.tolist(
-            np.zeros(len(joint_command_msg.names))
-        )
-
-        return joint_command_msg
-
-
+    # Callback functions
     def joy_callback(self, msg: Joy):
 
         self.joy_msg = msg
@@ -204,7 +103,7 @@ class VelCtrl:
             j = self._robot.jacob0(cur_js)
 
             # get joint velocities
-            joint_vel = VelCtrl.solve_RMRC(j, ee_vel)
+            joint_vel = solve_RMRC(j, ee_vel)
 
             # Declare the joint's limit speed (NOTE: For safety purposes, set this to a value below or equal to 0.6 rad/s. Speed range spans from 0.0-1.0 rad/s)
             limit_speed = 0.6
@@ -220,8 +119,6 @@ class VelCtrl:
 
             self.joint_command.velocity = np.ndarray.tolist(joint_vel)
 
-
-    # Extract the current joint states of Sawyer
     def js_callback(self, js: JointState):
         """
         JointState message definition:
@@ -236,9 +133,6 @@ class VelCtrl:
         """
         # Stores most recent joint states from the /robot/joint_states topic
         self.cur_config = js.position
-
-
-
 
     # Publishing velocity commands as a JointCommand message
     def pub_joint_ctrl_msg(self):
@@ -259,38 +153,78 @@ class VelCtrl:
 
             self._joint_comm_pub.publish(self.joint_command)
 
-    @staticmethod
-    def solve_RMRC(jacob, ee_vel):
-
-        # calculate manipulability
-        w = np.sqrt(np.linalg.det(jacob @ np.transpose(jacob)))
-
-        # set threshold and damping
-        w_thresh = 0.04
-        max_damp = 0.5
-
-        # if manipulability is less than threshold, add damping
-        if w < w_thresh:
-            damp = (1-np.power(w/w_thresh, 2)) * max_damp 
-        else: 
-            damp = 0
-
-        # calculate damped least square
-        j_dls = np.transpose(jacob) @ np.linalg.inv( jacob @ np.transpose(jacob) + damp * np.eye(6) )
-
-        # get joint velocities, if robot is in singularity, use damped least square
-        joint_vel = j_dls @ np.transpose(ee_vel)
-        return joint_vel
-
     def run_joystick_control(self):
 
         while not rospy.is_shutdown():
             self.pub_joint_ctrl_msg()
             rospy.sleep(0.1)
 
+    @staticmethod
+    def joint_command_init():
 
+        # Joint Command Message Initialisation
+        joint_command_msg = JointCommand()
 
-def main():
+        # To check the order of the joints run a 'rostopic echo /robot/joint_states', and assign the order displayed to the JointCommand().names argument
+        # This sequence is important as it is used to map the joint velocities to the correct joints, currently matched with actual Sawyer model instead of Gazebo version.
+        joint_command_msg.names = [
+            "head_pan",
+            "right_j0",
+            "right_j1",       
+            "right_j2",
+            "right_j3",
+            "right_j4",
+            "right_j5",
+            "right_j6",
+            "torso_t0",
+        ]
+
+        # Set the control mode to the VELOCITY
+        joint_command_msg.mode = VELOCITY_MODE
+
+        # Set the initial velocity at all joints to 0.0
+        joint_command_msg.velocity = np.ndarray.tolist(
+            np.zeros(len(joint_command_msg.names))
+        )
+
+        return joint_command_msg
+
+    @staticmethod
+    def gripper_ctrl_init():
+
+            # Initialise interfaces
+            rs = intera_interface.RobotEnable(CHECK_VERSION)
+            init_state = rs.state()
+            gripper = None
+            original_deadzone = None
+
+            rp = intera_interface.RobotParams()
+            valid_limbs = rp.get_limb_names()
+
+            # Cleaning function (executed on shutdown)
+            def clean_shutdown():
+                if gripper and original_deadzone:
+                    gripper.set_dead_zone(original_deadzone)
+
+            try:
+                # Instantiate the gripper object
+                gripper = intera_interface.Gripper(
+                    valid_limbs[0] + "_gripper")
+            except (ValueError, OSError) as e:
+                rospy.logerr(
+                    "Could not detect an electric gripper attached to the robot.")
+                clean_shutdown()
+                return
+            rospy.on_shutdown(clean_shutdown)
+
+            # Possible deadzone values: 0.001 - 0.002
+            original_deadzone = gripper.get_dead_zone()
+            gripper.set_dead_zone(0.001)
+
+            return gripper
+
+if __name__ == "__main__":
+
     try:
         # Initialise controller
         out = VelCtrl()
@@ -301,7 +235,3 @@ def main():
         rospy.spin()
     except rospy.ROSInterruptException as e:
         raise e
-
-
-if __name__ == "__main__":
-    main()
